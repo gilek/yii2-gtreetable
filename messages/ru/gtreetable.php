d240afa4c1bb63ea6fd67cb203d03e4920e2f79c
amstr1k <sferryf@gmail.com>
2015-04-08 01:10:29 +0300

    Added Russian language

---
+++ messages/ru/gtreetable.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @link      https://github.com/gilek/yii2-gtreetable
+ * @copyright Copyright (c) 2015 Maciej Kłak
+ * @license   https://github.com/gilek/yii2-gtreetable/blob/master/LICENSE
+ * @author    Sergey Fedorov sferryf@gmail.com
+ */
+
+return [
+  'Position indicated by related ID is not exists!' => 'Позиция на которую ссылается ID не существует',
+  'Adding operation `{name}` failed!'               => 'Операция добавления `{name}` не выполнена!',
+  'Unsupported insert position!'                    => 'Неподдерживаемая позиция добавления!',
+  'Position is not exists!'                         => 'Позиция не существует!',
+  'Main element can`t be deleted!'                  => 'Основная категория не может быть удалена!',
+  'Deleting operation `{name}` failed!'             => 'Операция удаления `{name}` не выполнена!',
+  'Updating operation `{name}` failed!'             => 'Операция обновления `{name}` не выполнена!',
+  'Name'                                            => 'Название'
+];
